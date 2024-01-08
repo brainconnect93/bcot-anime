4bb96263512db8b1d49c2d2451f7f0b3e04d0c74
brainconnect93 <brainconnect0@gmail.com>
2024-01-08 12:29:08 +0100

    implement the infinite scroll and add npm package

--- app/action.ts
+++ app/action.ts
@@ -1,10 +1,10 @@
 'use server'
 
 const fetchAnime = async(page: number) => {
-  const res = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`);
+  const res = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=12&order=popularity`);
 
   const data = await res.json();
-  
+
   return data;
 }
 
